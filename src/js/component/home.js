7580c27f26e6dffbe4b63ff0a92673a201981e83
Shar1799 <sharmey99@gmail.com>
2021-04-21 01:00:00 +0000

    First commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,112 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 export function Home() {
+	//Función para la lista de tareas
+	const [listaDeTareas, setListaDeTareas] = useState([]);
+
+	const MyFunction = e => {
+		if (e.key === "Enter") {
+			setListaDeTareas([
+				...listaDeTareas,
+				{ label: e.target.value, done: false }
+			]);
+			e.target.value = "";
+		}
+	};
+
+	const EliminarTarea = indexItem => {
+		setListaDeTareas(prevState =>
+			prevState.filter((todo, index) => index !== indexItem)
+		);
+	};
+
+	const EliminarTodo = async () => {
+		await fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/Shar1799",
+			{
+				method: "DELETE",
+				headers: { "Content-Type": "application/json" }
+			}
+		)
+			.then(response => response.json())
+			.then(data => console.log(data))
+			.catch(error => console.log(error));
+
+		await fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/Shar1799",
+			{
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify([])
+			}
+		)
+			.then(response => response.json())
+			.then(data => console.log(data))
+			.catch(error => console.log(error));
+
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/Shar1799", {
+			method: "GET",
+			headers: { "Content-Type": "application/json" }
+		})
+			.then(response => response.json())
+			.then(data => setListaDeTareas(data));
+	};
+
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/Shar1799", {
+			method: "GET",
+			headers: { "Content-Type": "application/json" }
+		})
+			.then(response => response.json())
+			.then(data => setListaDeTareas(data));
+	}, []);
+
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/Shar1799", {
+			method: "PUT",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(listaDeTareas)
+		});
+	}, [listaDeTareas]);
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<div className="text-center mt-5">
+				<form className="formulario">
+					<h1>TO DO LIST </h1>
+					<span>Añadir una tarea</span>
+					<input
+						type="text"
+						placeholder="Tarea nueva..."
+						onKeyDown={event => MyFunction(event)}
+						className="m-2"
+					/>
+				</form>
+			</div>
+
+			<ul className="list-group">
+				{listaDeTareas.map((item, index) => {
+					return (
+						<li
+							id="listitem"
+							className="list-group-item"
+							key={index}>
+							{item.label}{" "}
+							<span onClick={() => EliminarTarea(index)}>
+								<u>Borrar</u>
+							</span>
+						</li>
+					);
+				})}
+
+				<div className="container">
+					<div className="text-right">
+						<button type="button" onClick={() => EliminarTodo()}>
+							Eliminar todas las tareas
+						</button>
+					</div>
+				</div>
+			</ul>
 		</div>
 	);
 }
